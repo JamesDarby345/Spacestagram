995b1d0fbffe51a5454ce739f5898d2f0b1d098b
james <jedarby@shaw.ca>
2022-01-07 17:04:09 -0800

    client Hello World!

---
+++ client/src/index.tsx
@@ -0,0 +1,15 @@
+import React from 'react';
+import { render } from 'react-dom';
+import reportWebVitals from './reportWebVitals';
+
+render(
+  <React.StrictMode>
+    <div>hello world</div>
+  </React.StrictMode>,
+  document.getElementById('root')
+);
+
+// If you want to start measuring performance in your app, pass a function
+// to log results (for example: reportWebVitals(console.log))
+// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
+reportWebVitals();
