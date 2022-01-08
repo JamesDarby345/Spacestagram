260e3bd203658f3c4cd1171df395c0c0998a6dfc
james <jedarby@shaw.ca>
2022-01-08 10:33:01 -0800

    client changes to type data

--- client/src/sections/NASAImages/NASAImages.tsx
+++ client/src/sections/NASAImages/NASAImages.tsx
@@ -1,9 +1,16 @@
 import { server } from "../../lib/api";
+import { 
+  NASAImagesData, 
+  likeNASAImageData, 
+  likeNASAImageVariables,
+  unlikeNASAImageData,
+  unlikeNASAImageVariables
+} from "./types";
 
 const NASAIMAGES = `
   query NASAImages {
     NASAImages {
-      _id
+      id
       likes
       copyright
       date
@@ -14,17 +21,68 @@ const NASAIMAGES = `
     }
   }
 `;
+
+const LIKENASAIMAGE = `
+  mutation likeNASAImage($id: ID!) {
+    like(id: $id) {
+      id
+      likes
+    }
+  }
+`
+
+const UNLIKENASAIMAGE = `
+  mutation likeNASAImage($id: ID!) {
+    like(id: $id) {
+      id
+      likes
+    }
+  }
+`
 interface Props {
   title: string;
 }
 
 export const NASAImages = ({ title }: Props) => {
   const fetchNASAImages = async () =>{
-    const NASAImages = await server.fetch({ query: NASAIMAGES });
-    console.log(NASAImages);
+    const { data } = 
+      await server.fetch<NASAImagesData>({ query: NASAIMAGES });
+    console.log(data);
   }
-  return <div>
-          <h2>{title}</h2>
-          <button onClick={ fetchNASAImages }>Query NASA Images!</button>
-        </div>
+
+  const likeNASAImage = async () => {
+    const { data } = await server.fetch<likeNASAImageData,
+     likeNASAImageVariables>({
+       query: LIKENASAIMAGE,
+       variables: {
+         id: '61d8994b39fb054f6f21b294'
+       }
+     })
+     console.log(data);
+  };
+
+  const unlikeNASAImage = async () => {
+    const { data } = await server.fetch<unlikeNASAImageData,
+     unlikeNASAImageVariables>({
+       query: UNLIKENASAIMAGE,
+       variables: {
+         id: '61d8994b39fb054f6f21b294'
+       }
+     })
+  };
+
+  return ( 
+    <div>
+      <h2>{title}</h2>
+      <button onClick={ fetchNASAImages }>
+        Query NASA Images!
+      </button>
+      <button onClick={ likeNASAImage }>
+        Like a NASA Image!
+      </button>
+      <button onClick={ unlikeNASAImage }>
+        Unlike a NASA Image!
+      </button>
+    </div>
+  )
 }
---
+++ client/src/sections/NASAImages/types.ts
@@ -0,0 +1,32 @@
+interface NASAImage {
+  id: string;
+  likes: number;
+  copyright?: string;
+  date: string;
+  explanation: string;
+  hdurl: string;
+  media_type: string;
+  service_version: string;
+  title: string;
+  url: string;
+}
+
+export interface NASAImagesData {
+  NASAImages: NASAImage[];
+}
+
+export interface likeNASAImageData {
+  like: NASAImage;
+}
+
+export interface likeNASAImageVariables {
+  id: string;
+}
+
+export interface unlikeNASAImageData {
+  like: NASAImage;
+}
+
+export interface unlikeNASAImageVariables {
+  id: string;
+}
