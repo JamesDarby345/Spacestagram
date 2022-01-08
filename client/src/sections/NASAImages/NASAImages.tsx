e8e37f39fed0a72b70ed720b9d77fcec3b998cdd
james <jedarby@shaw.ca>
2022-01-08 08:50:00 -0800

    broken

--- client/src/sections/NASAImages/NASAImages.tsx
+++ client/src/sections/NASAImages/NASAImages.tsx
@@ -1,7 +1,30 @@
+import { server } from "../../lib/api";
+
+const NASAIMAGES = `
+  query NASAImages {
+    NASAImages {
+      _id
+      likes
+      copyright
+      date
+      explanation
+      hdurl
+      title
+      url
+    }
+  }
+`;
 interface Props {
   title: string;
 }
 
 export const NASAImages = ({ title }: Props) => {
-  return <h2>{title}</h2>
-};
+  const fetchNASAImages = async () =>{
+    const NASAImages = await server.fetch({ query: NASAIMAGES });
+    console.log(NASAImages);
+  }
+  return <div>
+          <h2>{title}</h2>
+          <button onClick={ fetchNASAImages }>Query NASA Images!</button>
+        </div>
+}
